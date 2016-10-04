3fbf9cb16b9db4dd54b44ca522126a1ee842937e
jamesmatanle <jkmatanle@gmail.com>
2016-10-03 19:07:46 -0500

    in the middle fo seomthing

---
+++ sample_app/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+end
